cd8062c85f43325b7a93af7441ff0a859d8d779d
Alexandre-Rx <pro.alexandreroux@gmail.com>
2026-04-13 12:40:58 +0200

    Refactor user management script for clarity

--- Scripts/scripts_debian/Linux/utilisateurs_linux.sh
+++ Scripts/scripts_debian/Linux/utilisateurs_linux.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #
 # Sous-menu pour gérer la navigation après chaque action
-function menu_secondaire
-{
+function menu_secondaire {
     echo "1 - Revenir au menu Utilisateurs"
     echo "2 - Revenir au menu principal"
     echo "q - Quitter le script"
     read -p "Quel est votre choix ? " choix_secondaire
 
     case $choix_secondaire in
-    
     1)
         log "Retour menu utilisateurs"
         echo "Vous retournez au menu Utilisateurs"
@@ -55,7 +53,6 @@ do
     read -p "Quel est votre choix ? : " choix
 
     case $choix in
-
     1)
         # Création de compte
         log "Initialisation création utilisateur client"
@@ -106,7 +103,7 @@ do
         then
             # L'option -r supprime aussi le dossier personnel de l'utilisateur
             ssh $ssh_user@$ip_client "sudo userdel -r $cible_username"
-            echo "L'utilisateur $target_username a été supprimé."
+            echo "L'utilisateur $cible_username a été supprimé."
             log "Succès suppression utilisateur $cible_username"
         else
             echo "Action annulée."
