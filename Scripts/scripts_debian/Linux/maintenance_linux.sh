1f8046b0cedb5c42451dae72311a858fba878019
Camille-Cybear <camille@cybear.fr>
2026-04-20 16:59:36 +0200

    Update maintenance_linux.sh

--- Scripts/scripts_debian/Linux/maintenance_linux.sh
+++ Scripts/scripts_debian/Linux/maintenance_linux.sh
@@ -166,6 +166,14 @@ do
         ;;
     5)
         log "Liste_Utilisateurs_Locaux"
+		liste=$(cat /etc/passwd | cut -d: -f1)
+		if [ $? -eq 0 ]
+		then
+    		echo "Les 5 derniers utilisateurs locaux :"
+    		echo "$liste" | tail -10
+		else
+    		echo "Récupération impossible"
+		fi
         ;;
     r)
         log "Retour arrière"
