ca90260cccab00bf6289b6270cc029bd032e8ee6
Camille-Cybear <camille@cybear.fr>
2026-04-22 15:49:52 +0200

    Update utilisateurs_linux.ps1

--- Scripts/scripts_windows_server/Linux/utilisateurs_linux.ps1
+++ Scripts/scripts_windows_server/Linux/utilisateurs_linux.ps1
@@ -1,12 +1,7 @@
-# Définition de la fonction de journalisation
-function Write-Log {
-    param([string]$Message)
-    $Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    Add-Content -Path "C:\logs\log_evt.log" -Value "[$Date] $Message"
-}
+. "./scripts_windows_server/utilitaire.ps1"
 
 # Sous-menu pour gérer la navigation
-function Show-MenuSecondaire {
+function MenuSecondaire {
     Write-Host "1 - Revenir au menu Utilisateurs"
     Write-Host "2 - Revenir au menu principal"
     Write-Host "q - Quitter le script"
@@ -34,12 +29,12 @@ function Show-MenuSecondaire {
         default {
             Write-Host "L'option choisie n'existe pas, veuillez recommencer" -ForegroundColor Red
             Start-Sleep -Seconds 1
-            Show-MenuSecondaire
+            MenuSecondaire
         }
     }
 }
 
-Write-Log "Demande sur utilisateurs"
+Log "Demande sur utilisateurs"
 Write-Host "Bienvenue dans la gestion des Utilisateurs" -ForegroundColor Green
 Start-Sleep -Seconds 1
 Clear-Host
@@ -57,7 +52,7 @@ while ($true) {
 
     switch ($choix) {
         '1' {
-            Write-Log "Initialisation création utilisateur client Linux"
+            Log "Initialisation création utilisateur client Linux"
             $cible_username = Read-Host "Saisissez le nom du nouvel utilisateur"
             
             # Vérification de l'existence du compte via SSH
@@ -70,20 +65,20 @@ while ($true) {
                 # $LASTEXITCODE capte la réussite (0) ou l'échec de la commande SSH
                 if ($LASTEXITCODE -eq 0) {
                     Write-Host "L'utilisateur $cible_username a été créé avec succès." -ForegroundColor Green
-                    Write-Log "Succès création utilisateur $cible_username"
+                    Log "Succès création utilisateur $cible_username"
                 } else {
                     Write-Host "Erreur critique : L'utilisateur n'a pas pu être créé." -ForegroundColor Red
-                    Write-Log "Erreur création utilisateur $cible_username (Échec SSH/Sudo)"
+                    Log "Erreur création utilisateur $cible_username (Échec SSH/Sudo)"
                 }
             } else {
                 Write-Host "Erreur : L'utilisateur $cible_username existe déjà sur la machine cible." -ForegroundColor Red
-                Write-Log "Erreur création utilisateur $cible_username (existe déjà)"
+                Log "Erreur création utilisateur $cible_username (existe déjà)"
             }
-            Show-MenuSecondaire
+            MenuSecondaire
         }
 
         '2' {
-            Write-Log "Initialisation changement de mot de passe client Linux"
+            Log "Initialisation changement de mot de passe client Linux"
             $cible_username = Read-Host "Saisissez le nom de l'utilisateur"
             $nouveau_password = Read-Host "Saisissez le nouveau mot de passe" 
             Write-Host ""
@@ -93,16 +88,16 @@ while ($true) {
 
             if ($LASTEXITCODE -eq 0) {
                 Write-Host "Le mot de passe de $cible_username a été mis à jour." -ForegroundColor Green
-                Write-Log "Succès changement mdp $cible_username"
+                Log "Succès changement mdp $cible_username"
             } else {
                 Write-Host "Erreur lors du changement de mot de passe." -ForegroundColor Red
-                Write-Log "Erreur changement mdp $cible_username"
+                Log "Erreur changement mdp $cible_username"
             }
-            Show-MenuSecondaire
+            MenuSecondaire
         }
 
         '3' {
-            Write-Log "Initiation suppression utilisateur client Linux"
+            Log "Initiation suppression utilisateur client Linux"
             $cible_username = Read-Host "Saisissez le nom de l'utilisateur à supprimer"
             $confirm = Read-Host "Êtes-vous sûr de vouloir supprimer $cible_username ? (o/n)"
 
@@ -112,27 +107,27 @@ while ($true) {
                 
                 if ($LASTEXITCODE -eq 0) {
                     Write-Host "L'utilisateur $cible_username a été supprimé." -ForegroundColor Green
-                    Write-Log "Succès suppression utilisateur $cible_username"
+                    Log "Succès suppression utilisateur $cible_username"
                 } else {
                     Write-Host "Erreur lors de la suppression." -ForegroundColor Red
-                    Write-Log "Erreur suppression utilisateur $cible_username"
+                    Log "Erreur suppression utilisateur $cible_username"
                 }
             } else {
                 Write-Host "Action annulée." -ForegroundColor Yellow
-                Write-Log "Annulation suppression utilisateur $cible_username"
+                Log "Annulation suppression utilisateur $cible_username"
             }
-            Show-MenuSecondaire
+            MenuSecondaire
         }
 
         '4' {
-            Write-Log "Retour au menu principal"
+            Log "Retour au menu principal"
             Write-Host "Vous revenez au menu principal" -ForegroundColor Cyan
             Start-Sleep -Seconds 1
             exit 0
         }
 
         'q' {
-            Write-Log "Quitte le script"
+            Log "Quitte le script"
             Write-Host "Vous quittez le script" -ForegroundColor Cyan
             Start-Sleep -Seconds 1
             exit 50
