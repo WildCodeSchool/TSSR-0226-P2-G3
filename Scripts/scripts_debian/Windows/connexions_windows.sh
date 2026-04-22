981b5613046f760f5fc2ef40d6ba9049fc31a8b6
Camille-Cybear <camille@cybear.fr>
2026-04-22 17:52:50 +0200

    Update connexions_windows.sh

--- Scripts/scripts_debian/Windows/connexions_windows.sh
+++ Scripts/scripts_debian/Windows/connexions_windows.sh
@@ -1,52 +1,46 @@
 #!/bin/bash
 
-function log
-{
-    echo "$(date '+%d-%m-%Y_%H:%M:%S')_${USER}_$1" >> "$log_file"
-}
-
-# Il est nécessaire d'installer ipcalc sur le client linux (A noter dans install ou readme)
 
 #initialisation des variables principales
 
-last_logins=$(ssh $ssh_user@$ip_client "powershell.exe -Command 'Get-EventLog -LogName Security -InstanceId 4624 -Newest 5 | Select-Object TimeGenerated, UserName'")
-ipcon_client=$(ssh $ssh_user@$ip_client "powershell.exe -Command 'Get-NetIPAddress -AddressFamily IPv4 | Where-Object {\$_.InterfaceAlias -notlike ''*Loopback*''} | Select-Object -ExpandProperty IPAddress'")
-mask_client=$(ssh $ssh_user@$ip_client "powershell.exe -Command ")
-gateway_client=$(ssh $ssh_user@$ip_client "powershell.exe -Command ")
+last_logins=$(ssh $ssh_client "powershell.exe -Command 'Get-EventLog -LogName Security -InstanceId 4624 -Newest 5 | Select-Object TimeGenerated, UserName'")
+ipcon_client=$(ssh $ssh_client "powershell.exe -Command 'Get-NetIPAddress -AddressFamily IPv4 | Where-Object {\$_.InterfaceAlias -notlike ''*Loopback*''} | Select-Object -ExpandProperty IPAddress'")
+mask_client=$(ssh $ssh_client "powershell.exe -Command ")
+gateway_client=$(ssh $ssh_client "powershell.exe -Command ")
 
 
 
-function secondary_menu
+function menu_secondaire
 {
     echo "1 - Revenir au menu Disques"
     echo "2 - Revenir au menu principal"
     echo "q - Quitter le script"
-    read -p "Quel est votre choix ?" choice_secondary
+    read -p "Quel est votre choix ?" choix_secondaire
 
-    case $choice_secondary in
+    case $choix_secondaire in
 
     1)
         log "Retour menu connexion"
         echo "Vous retournez au menu connexion"
-        sleep 3
+        sleep 1
         return
         ;;
     2)
         log "Retour au menu principal"
         echo "Vous retournez au menu principal"
-        sleep 3
+        sleep 1
         exit 0
         ;;
     q)
         log "Quitte le script"
         echo "Vous quittez le script"
-        sleep 3
+        sleep 1
         exit 50
         ;;
     *)
         echo "L'option choisi n'existe pas, veuillez recommencer"
-        sleep 3
-        secondary_menu
+        sleep 1
+        menu_secondaire
         ;;
     esac
 }
@@ -54,7 +48,7 @@ function secondary_menu
 log "Demande sur connexion"
 
 echo "Bienvenue dans le menu connexion"
-sleep 3
+sleep 1
 clear
 
 # Menu Connexion
@@ -67,9 +61,9 @@ do
     echo "2 - Adresse IP, masque IP et passerelle du Client"
     echo "3 - Revenir au menu principal"
     echo "q - quitter le script"
-    read -p "Quel est votre choix ? :" choice
+    read -p "Quel est votre choix ? :" choix
 
-    case $choice in
+    case $choix in
 
     # Affichage des 5 dcerniers logins
 
@@ -77,7 +71,7 @@ do
 	    log "cinq derniers login"
 	    echo "Voici les 5 derniers loggings :"
 	    echo "$last_logins"
-	    secondary_menu
+	    menu_secondaire
         ;;
 # Adresse IP, Masque, Passerelle client
     2) 
@@ -85,7 +79,7 @@ do
 	    echo "L'adresse IP du client est $ipcon_client"
 	    echo "Le masque de sous-réseau du client est $mask_client"
 	    echo "La passerelle du client est $gateway_client"
-	    secondary_menu
+	    menu_secondaire
         ;;
     3)
 	    log "Retour arrière"
