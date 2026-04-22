f782d192a4566ead0eaa82c12ec7bdb00545a7c0
Camille-Cybear <camille@cybear.fr>
2026-04-22 22:09:24 +0200

    Update connexions_windows.sh

--- Scripts/scripts_debian/Windows/connexions_windows.sh
+++ Scripts/scripts_debian/Windows/connexions_windows.sh
@@ -3,7 +3,7 @@
 
 #initialisation des variables principales
 
-last_logins=$(ssh $ssh_client "powershell.exe -Command 'Get-EventLog -LogName Security -InstanceId 4624 -Newest 20 | Where-Object {\$_.ReplacementStrings[8] -eq \"2\" -or \$_.ReplacementStrings[8] -eq \"10\"} | Select-Object -First 5 TimeGenerated, @{Name=\"UserName\";Expression={\$_.ReplacementStrings[5]}}'")
+last_logins=$(ssh $ssh_client "powershell.exe -Command 'Get-EventLog -LogName Security -InstanceId 4624 -Newest 5 | Select-Object TimeGenerated, @{Name=\"UserName\";Expression={\$_.ReplacementStrings[5]}}'")
 ipcon_client=$(ssh $ssh_client "powershell.exe -Command 'Get-NetIPAddress -AddressFamily IPv4 | Where-Object {\$_.InterfaceAlias -notlike ''*Loopback*''} | Select-Object -ExpandProperty IPAddress'")
 $prefixe = ssh $ssh_client "powershell.exe -Command 'Get-NetIPAddress -AddressFamily IPv4 | Where-Object {\$_.InterfaceAlias -notlike ''*Loopback*''} | Select-Object -ExpandProperty PrefixLength'"
 mask_client=$(ssh $ssh_client "powershell.exe -Command '\$prefix = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {\$_.InterfaceAlias -notlike ''*Loopback*''}).PrefixLength; \$mask = [uint32]0; for(\$i=0;\$i -lt \$prefix;\$i++){\$mask = \$mask -bor (1 -shl (31-\$i))}; ([System.Net.IPAddress]\$mask).ToString()'")
