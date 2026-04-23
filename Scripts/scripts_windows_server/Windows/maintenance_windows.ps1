a00da2ff2d5c909c8be4fa046e4b26040a75fbe4
JeMa9701 <Jeremy-machado@outlook.fr>
2026-04-24 01:06:18 +0200

    Update maintenance_windows.ps1

--- Scripts/scripts_windows_server/Windows/maintenance_windows.ps1
+++ Scripts/scripts_windows_server/Windows/maintenance_windows.ps1
@@ -16,23 +16,27 @@ function MenuSecondaire {
             Log "Retour menu maintenance"
             Write-Host "Vous retournez au menu maintenance"
             Start-Sleep -Seconds 1
+            Clear-Host
             return
         }
         "2" {
             Log "Retour au menu principal"
             Write-Host "Vous retournez au menu principal"
             Start-Sleep -Seconds 1
+            Clear-Host
             exit 0
         }
         "q" {
             Log "Quitte le script"
             Write-Host "Vous quittez le script"
             Start-Sleep -Seconds 1
+            Clear-Host
             exit 50
         }
         default {
             Write-Host "L'option choisie n'existe pas, veuillez recommencer"
             Start-Sleep -Seconds 1
+            Clear-Host
             MenuSecondaire
         }
     }
