1738a15b7aff3e3a6c3f36ec5bbc4817d6ec1139
Camille-Cybear <camille@cybear.fr>
2026-04-23 09:02:13 +0200

    Update disques_windows.ps1

--- Scripts/scripts_windows_server/Windows/disques_windows.ps1
+++ Scripts/scripts_windows_server/Windows/disques_windows.ps1
@@ -1,4 +1,4 @@
-. "./scripts_windows_server/utilitaire.ps1"
+. "$PSScriptRoot\scripts_windows_server\utilitaire.ps1"
 
 # Variables principales - commandes PowerShell pour client Windows
 $nombre_disque_client = ssh $ssh_client "powershell.exe -Command '(Get-Disk | Measure-Object).Count'"
